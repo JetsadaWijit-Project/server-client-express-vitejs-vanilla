ac3b344bd2450e34f7226c953f1b72455f8a1cff
Jetsada Wijit <jetsadawijit@outlook.com>
2025-05-31 20:31:14 +0000

    feat: Add cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json()); // Enable JSON parsing
 
 // Example GET endpoint
