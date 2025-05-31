041e3a23f46dacefe29e18f09c699b2de8d64194
Jetsada Wijit <jetsadawijit@outlook.com>
2025-05-31 21:01:59 +0000

    Del log

--- run.bat
+++ run.bat
@@ -1,12 +1,4 @@
 @echo off
-:: Create logs directory if it doesn't exist
-if not exist logs mkdir logs
-
-:: Get timestamp in format yyyy-MM-dd-HH-mm-ss
-for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd-HH-mm-ss"') do set timestamp=%%i
-
-:: Set log prefix path
-set LOG_PREFIX=logs\%timestamp%
 
 echo ========================================
 echo Fetching latest changes from Git...
